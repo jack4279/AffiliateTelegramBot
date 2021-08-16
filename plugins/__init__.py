8afd4f4fbb07cbe48436a33821e20b76ba3454b8
jack4279 <88567670+jack4279@users.noreply.github.com>
2021-08-16 15:43:43 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -4,4 +4,8 @@ BITLY_ACCESS_TOKEN ="4fae9902744ef33f75a12df129858928f6c9b663"
 
 access = bitly_api.Connection(access_token = BITLY_ACCESS_TOKEN)
 
+full_link = newReferURL(pcode)
+
+short_url = access.shorten(full_link)
+
 newReferURL(pcode)
