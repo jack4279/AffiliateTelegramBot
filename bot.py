1913298807cd1ed8913f87db178275b6436caf27
jack4279 <88567670+jack4279@users.noreply.github.com>
2021-08-18 16:27:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,12 +44,10 @@ def start(update, context):
 
 # Create the new URL with the refer tag
 def newReferURL(pcode, update):
-    msg = update.message.text 
-    thestring = msg 
+    msg = update.message.text
+    thestring = msg
     URLless_string = re.sub(r'^https?:\/\/.*[\r\n]*', '', thestring, flags=re.MULTILINE)
-    long_urls = ["https://"+baseURL+pcode+"?tag="+affiliate_tag]
-    short_url = shortener.shorten_urls(long_urls)
-    return shortener.shorten_urls(["https://"+baseURL+pcode+"?tag="+affiliate_tag])
+    return URLless_string+"\nhttps://"+baseURL+pcode+"?tag="+affiliate_tag
 
 #Expand shorted URL (amzn.to links) to normal Amazon URL
 def unshortURL(url):
