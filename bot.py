1c00490a0bcb06a04efb763c27da0674a65aedbe
jack4279 <88567670+jack4279@users.noreply.github.com>
2021-08-18 14:30:08 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -70,8 +70,10 @@ def filterText(update, context):
         m = re.search(r'(?:dp\/[\w]*)|(?:gp\/product\/[\w]*)',msg[start:].split(" ")[0])
         if m != None:
             pCode = m.group(0)
-        context.bot.send_message(chat_id=update.message.chat_id,reply_to_message_id=update.message.message_id, text=newReferURL(pCode, update))
-
+        context.bot.send_message(chat_id=update.message.chat_id,reply_to_message_id=update.message.message_id, text=short_url[0])
+        url = newReferURL(pCode, update)
+        long_urls = [msg]
+        short_url=shortener.shorten_urls(long_urls)
 def main():
     """Start the bot."""
     # Create the Updater and pass it your bot's token.
